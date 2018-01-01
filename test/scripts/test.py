7918e8de8daffdda74663fbe0efddbe2320c6a2c
jqin <qinzhiqiang9@163.com>
2018-01-02 01:05:12 +0800

    update test script

--- test/scripts/test.py
+++ test/scripts/test.py
@@ -1,7 +1,7 @@
-
+import os
 from src.scripts.listtest import DataArray
 
-dataList = DataArray()
+dataList = DataArray(datafiledir=os.path.join('..' + os.sep + '..' + os.sep + 'src' + os.sep + 'data'))
 
 rows = dataList.get_top_list(skip=100, limit=100)
 for row in rows:
