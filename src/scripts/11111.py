b953bbe93422cd68fac315bd413716555ebc44e3
jqin <qinzhiqiang9@163.com>
2018-01-03 12:00:16 +0800

    update __init__

--- src/scripts/11111.py
+++ src/scripts/11111.py
@@ -1,11 +1,15 @@
 import numpy as np
-from src.scripts.superlist import super_list
+from src.scripts.superlist import SuperList
 
-dataList = super_list()
-exceptions = dataList.get_top_list(f=lambda r:r[4] == str(1))
-# dataList.print_summary_list(exceptions)
+dataList = SuperList()
+dataList.load()
+#exceptions = dataList.get_top_list(f=lambda r: r[4] == str(1))
+#dataList.print_summary_list(exceptions)
 
-dataList.print_summary_list(np.array(dataList.filecontent)[:,1:2])
+# print(dataList.__len__())
+print(len(dataList))
+
+#dataList.print_summary_list(np.array(dataList.filecontent)[:,1:2])
 #print(allException)
 # print(dataList.get_content_len())
 # dataList.group_by_coulumn()
--- src/scripts/superlist.py
+++ src/scripts/superlist.py
@@ -10,47 +10,44 @@ import collections
 import pprint
 
 
-class super_list:
-    datafiledir = None
-    datafilename = None
-    haveheader = True
-    fileheader = []
-    filecontent = []
-    filelenth = 0
-
+class SuperList:
 
     def __init__(self, datafiledir=os.path.join('..' + os.sep + 'data'), datafilename='train.csv', haveheader=True):
         self.datafiledir = datafiledir
         self.datafilename = datafilename
         self.haveheader = haveheader
+        self.fileheader = []
+        self.filecontent = []
 
-        datafile = open(self.datafiledir + os.sep + self.datafilename)
-        fileReader = csv.reader(datafile)
-        for row in fileReader:
-            row.insert(0, fileReader.line_num)  # insert row number at first column
-            if self.haveheader is True and fileReader.line_num is 1:
-                self.fileheader = row
-            else:
-                row[2] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(float(row[2])))
-                self.filecontent.append(row)
-        self.filelength = len(self.filecontent)
-        datafile.close()
+    def load(self):
+        with open(self.datafiledir + os.sep + self.datafilename) as datafile:
+            filereader = csv.reader(datafile)
+            for row in filereader:
+                row.insert(0, filereader.line_num)  # insert row number at first column
+                if self.haveheader is True and filereader.line_num is 1:
+                    self.fileheader = row
+                else:
+                    row[2] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(float(row[2])))
+                    self.filecontent.append(row)
 
-    def get_file_header(self):
+    @property
+    def fileheader(self):
         return self.fileheader
 
-    def get_file_content(self):
+    @property
+    def filecontent(self):
         return self.filecontent
 
-    def get_top_list(self, skip=0, limit=None, f=lambda row:True):
-        filteredlist = list(filter(f,self.filecontent))
+    def get_top_list(self, skip=0, limit=None, f=lambda row: True):
+        filteredlist = list(filter(f, self.filecontent))
         limitedlist = filteredlist[skip:len(filteredlist) if limit is None else limit + skip]
         return limitedlist
 
-    def get_content_len(self):
+    def __len__(self):
         return len(self.filecontent)
 
     def group_by_coulumn(self, columnNum=0):
+        [row[0] for row in self.filecontent]
 
         a = np.array(self.filecontent)
 
@@ -58,7 +55,8 @@ class super_list:
         for k in d.keys():
             print(k)
 
-    def print_summary_list(self, ls=[]):
+    @staticmethod
+    def print_summary_list(ls=[]):
         pprint.pprint(ls)
         print('Amount for target list is ' + str(len(ls)))
 
